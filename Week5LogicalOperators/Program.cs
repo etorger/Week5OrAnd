71fd4054664b19fa6e1747e334a7dbb5cc46da75
etorger <etorger@tktk.ee>
2022-10-02 12:24:11 +0300

    add week 5 code

---
+++ Week5LogicalOperators/Program.cs
@@ -0,0 +1,12 @@
+﻿// and - &&
+//true && true --> true
+//true && false --> false
+//false && true --> false
+//false & false --> false
+//kõik tingimused peavad olema täidetud
+
+//OR - || (pipes) 
+//true || true --> true
+//true || false --> true
+//false || true --> true
+//false || false --> false
